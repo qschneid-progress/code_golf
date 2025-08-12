9fd2918b0fc3dda166a4de058143b081c85a73ef
Quaker Schneider <qschneid@progress.com>
2025-08-12 16:27:12 -0400

    submission #1

---
+++ 177.py
@@ -0,0 +1,9 @@
+import sys
+o = ''
+for x in open(sys.argv[1]).read()[::-1].split('\n'):
+    for s, m, l, n in zip('IXCM', 'VLD.', 'XCM.', x):
+        n = int(n)
+        f = n % 5
+        o += s * f * (f < 4) + (m if n < 9 else l) * (n > 3) + s * (f == 4)
+    o += '\n'
+print(o[::-1])
