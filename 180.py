7db81d9573a5136ba21dd3919533375cb8307202
Quaker Schneider <qschneid@progress.com>
2025-08-12 16:46:15 -0400

    Update and rename 177.py to 180.py

--- 180.py
+++ 180.py
@@ -6,4 +6,4 @@ for x in open(sys.argv[1]).read()[::-1].split('\n'):
         f = n % 5
         o += s * f * (f < 4) + (m if n < 9 else l) * (n > 3) + s * (f == 4)
     o += '\n'
-print(o[::-1])
+print(o[-2:1:-1])
