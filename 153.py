15f429f8b312b7244b2cf7ba85a4a2ad660a8fd2
Quaker Schneider <qschneid@progress.com>
2025-08-13 14:45:45 -0400

    swap to tabs

--- 153.py
+++ 153.py
@@ -1,8 +1,8 @@
 import sys
 for x in open(sys.argv[1]):
-    o=''
-    for s,m,l,n in zip('IXCM','VLD.','XCM.',x[:-1][::-1]):
-        n=int(n)+1
-        f=n%5-1
-        o+=s*f+(m if n<10 else l)*(n>4)+s*(-f)
-    print(o[::-1])
+	o=''
+	for s,m,l,n in zip('IXCM','VLD.','XCM.',x[:-1][::-1]):
+		n=int(n)+1
+		f=n%5-1
+		o+=s*f+(m if n<10 else l)*(n>4)+s*(-f)
+	print(o[::-1])
