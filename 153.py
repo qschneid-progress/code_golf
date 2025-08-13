aacddb9cbe0219722a641dc85d743beb07823433
Quaker Schneider <qschneid@progress.com>
2025-08-13 14:41:42 -0400

    Update and rename 154.py to 153.py

--- 153.py
+++ 153.py
@@ -2,7 +2,7 @@ import sys
 for x in open(sys.argv[1]):
     o=''
     for s,m,l,n in zip('IXCM','VLD.','XCM.',x[:-1][::-1]):
-        n=int(n)
-        f=(n+1)%5-1
-        o+=s*f+(m if n<9 else l)*(n>3)+s*(-f)
+        n=int(n)+1
+        f=n%5-1
+        o+=s*f+(m if n<10 else l)*(n>4)+s*(-f)
     print(o[::-1])
