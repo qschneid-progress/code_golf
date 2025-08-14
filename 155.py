a455133907f621493741eb082be3b15b37a71341
Quaker Schneider <qschneid@progress.com>
2025-08-13 20:36:10 -0400

    Create 155.py

---
+++ 155.py
@@ -0,0 +1,8 @@
+import sys
+for x in open(sys.argv[1]):
+	o=''
+	for s,m,l,n in zip('IXCM','VLD.','XCM.',x.strip()[::-1]):
+		n=int(n)+1
+		f=n%5-1
+		o+=s*f+(l if n>9 else m)*(n>4)+s*(-f)
+	print(o[::-1])
